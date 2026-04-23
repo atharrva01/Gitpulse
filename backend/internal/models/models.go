68369b652446475e46bd58e9a40c9cb137617f43
atharrva01 <atharvaborade568@gmail.com>
2026-04-23 23:47:03 +0530

    backend: models for wrapped, maintainer, contributors

--- backend/internal/models/models.go
+++ backend/internal/models/models.go
@@ -73,6 +73,72 @@ type StreakDay struct {
 	HasReview bool      `db:"has_review" json:"has_review"`
 }
 
+type WatchedRepo struct {
+	ID           int64     `db:"id" json:"id"`
+	UserID       int64     `db:"user_id" json:"user_id"`
+	RepoFullName string    `db:"repo_full_name" json:"repo_full_name"`
+	AddedAt      time.Time `db:"added_at" json:"added_at"`
+}
+
+type RepoContributor struct {
+	ID             int64      `db:"id" json:"id"`
+	WatchedRepoID  int64      `db:"watched_repo_id" json:"watched_repo_id"`
+	Login          string     `db:"login" json:"login"`
+	AvatarURL      string     `db:"avatar_url" json:"avatar_url"`
+	PRCount30d     int        `db:"pr_count_30d" json:"pr_count_30d"`
+	PRCount90d     int        `db:"pr_count_90d" json:"pr_count_90d"`
+	LastPRAt       *time.Time `db:"last_pr_at" json:"last_pr_at"`
+	FirstPRAt      *time.Time `db:"first_pr_at" json:"first_pr_at"`
+	IsStale        bool       `db:"is_stale" json:"is_stale"`
+}
+
+type StalePR struct {
+	ID             int64     `db:"id" json:"id"`
+	WatchedRepoID  int64     `db:"watched_repo_id" json:"watched_repo_id"`
+	GitHubPRID     int64     `db:"github_pr_id" json:"github_pr_id"`
+	Number         int       `db:"number" json:"number"`
+	Title          string    `db:"title" json:"title"`
+	AuthorLogin    string    `db:"author_login" json:"author_login"`
+	HTMLURL        string    `db:"html_url" json:"html_url"`
+	OpenedAt       time.Time `db:"opened_at" json:"opened_at"`
+	DaysOpen       int       `db:"days_open" json:"days_open"`
+}
+
+type MaintainerDashboard struct {
+	Repo         WatchedRepo       `json:"repo"`
+	Contributors []RepoContributor `json:"contributors"`
+	StalePRs     []StalePR         `json:"stale_prs"`
+	TotalOpen    int               `json:"total_open_prs"`
+}
+
+type WrappedStats struct {
+	Year            int            `json:"year"`
+	Login           string         `json:"login"`
+	Name            string         `json:"name"`
+	AvatarURL       string         `json:"avatar_url"`
+	ImpactScore     int            `json:"impact_score"`
+	TotalPRs        int            `json:"total_prs"`
+	TotalReviews    int            `json:"total_reviews"`
+	TotalAdditions  int            `json:"total_additions"`
+	TotalDeletions  int            `json:"total_deletions"`
+	UniqueRepos     int            `json:"unique_repos"`
+	LongestStreak   int            `json:"longest_streak"`
+	TopRepo         string         `json:"top_repo"`
+	TopRepoPRs      int            `json:"top_repo_prs"`
+	MostActiveMonth string         `json:"most_active_month"`
+	MostActivePRs   int            `json:"most_active_month_prs"`
+	MonthlyActivity []MonthlyCount `json:"monthly_activity"`
+	FirstPRTitle    string         `json:"first_pr_title"`
+	FirstPRRepo     string         `json:"first_pr_repo"`
+	LastPRTitle     string         `json:"last_pr_title"`
+	LastPRRepo      string         `json:"last_pr_repo"`
+}
+
+type MonthlyCount struct {
+	Month string `json:"month"`
+	Count int    `json:"count"`
+}
+
 type DashboardStats struct {
 	User             *User        `json:"user"`
 	TotalMergedPRs   int          `json:"total_merged_prs"`
